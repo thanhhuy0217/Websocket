478fb5e4f86b150a195b28df5b63a60003613fef
hnlanh <hnlananh06@gmail.com>
2025-11-19 17:19:09 +0700

    Đổi interface.h thành ICommandExcutor.h và up lên file ApplicationModule

---
+++ WebSocker_1/Application.cpp
@@ -0,0 +1,5 @@
+#include"ApplicationModule.h"
+#include<cstdlib>
+#include<sstream>
+#include<algorithm>
+#include<cctype>
---
+++ WebSocker_1/ApplicationModule.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+#include<algorithm>
+
+struct ApplicationEntry {
+	std::string id; //id nội bộ
+	std::string displayName; //tên hiện thị lên UI
+	std::string exeName; //Tên file exe
+	std::string startCommand; //Lệnh để start
+};
+
+class ApplicationModule {
+private:
+	std::vector<ApplicationEntry> _apps;
+	const std::vector<ApplicationEntry>* findApp(const std::string& appIdorName) const;
+public:
+	std::string ListApplication();
+	bool StartApplication(const std::string& appName);
+	bool EndApplication(const std::string& appName);
+public:
+	static std::string toLower(std::string s) {
+		std::transform(s.begin(), s.end(),s.begin(), [](unsigned char c) { return std::tolower(c); });
+		return s;
+	}
+};
